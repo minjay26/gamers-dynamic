51abc240914be25ba40461772587e8e648e828e8
minjay26 <min.luo@tendata.cn>
2019-09-12 14:59:37 +0800

    增加es搜索高亮

--- gamers-dynamic-core/src/main/java/org/minjay/gamers/dynamic/data/elasticsearch/repository/DynamicRepositoryImpl.java
+++ gamers-dynamic-core/src/main/java/org/minjay/gamers/dynamic/data/elasticsearch/repository/DynamicRepositoryImpl.java
@@ -1,18 +1,29 @@
 package org.minjay.gamers.dynamic.data.elasticsearch.repository;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.StringUtils;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.GeoDistanceQueryBuilder;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
 import org.minjay.gamers.dynamic.data.elasticsearch.domain.Dynamic;
 import org.minjay.gamers.dynamic.service.model.SearchCriteria;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+import org.springframework.data.elasticsearch.core.SearchResultMapper;
+import org.springframework.data.elasticsearch.core.aggregation.AggregatedPage;
+import org.springframework.data.elasticsearch.core.aggregation.impl.AggregatedPageImpl;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.data.elasticsearch.core.query.SearchQuery;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 import static org.elasticsearch.index.query.QueryBuilders.fuzzyQuery;
 import static org.elasticsearch.index.query.QueryBuilders.termQuery;
@@ -52,12 +63,41 @@ public class DynamicRepositoryImpl implements DynamicRepositoryCustom {
         SearchQuery searchQuery = new NativeSearchQueryBuilder()
                 .withIndices("dynamic")
                 .withTypes("dynamic")
+                .withHighlightFields(new HighlightBuilder.Field("content"), new HighlightBuilder.Field("username"))
                 .withQuery(builder)
                 .withFilter(geoDistanceQueryBuilder)
                 .withSearchType(SearchType.QUERY_THEN_FETCH)
                 .withPageable(pageable)
                 .build();
-        return elasticsearchTemplate.queryForList(searchQuery, Dynamic.class);
+        return elasticsearchTemplate.queryForPage(searchQuery, Dynamic.class, new SearchResultMapper() {
+            @Override
+            public <T> AggregatedPage<T> mapResults(SearchResponse response, Class<T> clazz, Pageable pageable) {
+                if (response.getHits().getHits().length < 1) {
+                    return new AggregatedPageImpl<>(Collections.EMPTY_LIST);
+                }
+                List<Dynamic> dynamics = new ArrayList<>();
+                ObjectMapper mapper = new ObjectMapper();
+                for (SearchHit searchHit : response.getHits()) {
+                    Dynamic dynamic = null;
+                    try {
+                        dynamic = mapper.readValue(searchHit.getSourceAsString(), Dynamic.class);
+                    } catch (Exception ex) {
+
+                    }
+                    dynamic.setId(searchHit.getId());
+                    HighlightField highlightContent = searchHit.getHighlightFields().get("content");
+                    if (highlightContent != null) {
+                        dynamic.setContent(highlightContent.fragments()[0].toString());
+                    }
+                    HighlightField usernameContent = searchHit.getHighlightFields().get("username");
+                    if (usernameContent != null) {
+                        dynamic.setContent(usernameContent.fragments()[0].toString());
+                    }
+                    dynamics.add(dynamic);
+                }
+                return new AggregatedPageImpl<>((List<T>) dynamics);
+            }
+        }).getContent();
     }
 
     private String getWildcardString(String value) {
